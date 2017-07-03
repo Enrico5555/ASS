32a2efce2456bf3db35d6d8c60f2b1588146d1b4
tete94 <enricobarbieric@gmail.com>
2017-07-03 22:23:06 +0000

    Added threading + small interface.

--- server.py
+++ server.py
@@ -1,20 +1,41 @@
-#!/usr/bin/python
+#!/usr/bin/python3.5
 
 import socket
-
+import threading
 import os, sys
 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+running = True
+
+def server_loop():
+	while running:
+		(client_socket, address) = server_socket.accept()
+		# create thread to receive new messages
+	server_socket.close()
+
 def main():
-	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	# setup
+	as_ip = str(raw_input('Escriba la IP del sistema autonomo: '))
+	as_mask = str(raw_input('Escriba la mascara: '))
+	as_number = int(raw_input('Escriba el numero de sistema autonomo: '))
+
 	server_socket.bind(('0.0.0.0', 57809))
 	server_socket.listen(5)
 
-	while True:
-		(client_socket, address) = server_socket.accept()
-		#ct = client_thread(client_socket)
-		#ct.run()
+	thread = threading.Thread(target=server_loop)
+	thread.daemon = True
+	thread.start()
 
-	server_socket.close()
+	choice = -1
+	while choice != 0:
+		choice = int(raw_input('Que desea hacer?\n1 - Agregar vecino.\n2 - Salir\n'))
+		if choice == 0:
+			# TODO: close socket
+			running = False
+		if choice == 1:
+			vc_ip = str(raw_input('Escriba la IP del vecino'))
+			vc_mask = str(raw_input('Escriba la mascara del vecino: '))
+			vc_number = str(raw_input('Escriba el numero de sistema autonomo vecino: '))
 
 if __name__ == "__main__":
 	#esto corre de primero    
